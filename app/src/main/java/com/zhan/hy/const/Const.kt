47d0f5e3c6ca01ba6cc7b5e19274c8486073e937
HyJame <1063523767@qq.com>
2019-09-21 10:13:43 +0800

    init project

---
+++ app/src/main/java/com/zhan/hy/const/Const.kt
@@ -0,0 +1,11 @@
+package com.zhan.hy.const
+
+/**
+ *  @author: HyJame
+ *  @date:   2019-09-21
+ *  @desc:   TODO
+ */
+object Const {
+
+    const val KT_ARMOR = "KtArmor"
+}
---
+++ app/src/main/java/com/zhan/hy/ext/Common.kt
@@ -0,0 +1,15 @@
+package com.zhan.hy.ext
+
+/**
+ * @author  hyzhan
+ * @date    2019/5/22
+ * @desc    TODO
+ */
+inline fun tryCatch(tryBlock: () -> Unit, catchBlock: (Throwable) -> Unit = {}) {
+    try {
+        tryBlock()
+    } catch (t: Throwable) {
+        t.printStackTrace()
+        catchBlock(t)
+    }
+}
