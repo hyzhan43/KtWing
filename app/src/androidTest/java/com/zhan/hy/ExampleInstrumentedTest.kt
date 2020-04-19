1d24d04a602dcbf61d00a3560231d889e76b616a
HyJame <1063523767@qq.com>
2020-04-19 12:16:26 +0800

    add startActivityAndFinish ext

--- app/src/androidTest/java/com/zhan/hy/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/zhan/hy/ExampleInstrumentedTest.kt
@@ -1,7 +1,5 @@
 package com.zhan.hy
 
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.runner.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -13,12 +11,9 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.zhan.hy", appContext.packageName)
     }
 }
--- wing/src/main/java/com/zhan/ktwing/ext/Activity.kt
+++ wing/src/main/java/com/zhan/ktwing/ext/Activity.kt
@@ -7,14 +7,26 @@ import androidx.fragment.app.Fragment
 import com.zhan.ktwing.utils.IntentUtils
 
 /**
- * author：  HyZhan
+ * author：  HyJame
  * create：  2019/8/1
- * desc：    TODO
+ * desc：    activity 扩展方法
+ */
+
+/**
+ *  启动 Activity
  */
 inline fun <reified T : Activity> Context.startActivity(vararg params: Pair<String, Any?>) {
     val intent = Intent(this, T::class.java)
     if (params.isNotEmpty()) IntentUtils.fillIntentArguments(intent, params)
     this.startActivity(intent)
+}
+
+/**
+ * 启动 Activity, 并且 finish
+ */
+inline fun <reified T : Activity> Activity.startActivityAndFinish(vararg params: Pair<String, Any?>) {
+    this.startActivity<T>(*params)
+    this.finish()
 }
 
 inline fun <reified T : Activity> Fragment.startActivity(vararg params: Pair<String, Any?>) {
